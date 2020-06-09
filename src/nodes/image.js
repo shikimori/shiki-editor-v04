98efc566933d0f99dd0ebfa06832216dbedf4799
Andrey Sidorov <takandar@gmail.com>
2020-06-09 22:41:45 +0300

    add delete control

--- src/nodes/image.js
+++ src/nodes/image.js
@@ -26,7 +26,12 @@ export default class Image extends Node {
       toDOM: node => [
         'a',
         { class: 'b-image unprocessed', href: node.attrs.src },
-        ['img', node.attrs]
+        ['img', node.attrs],
+        [
+          'div',
+          { class: 'controls' },
+          ['div', { class: 'delete' }]
+        ]
       ]
     };
   }
