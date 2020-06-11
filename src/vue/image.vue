58dfb0169917b3802988937072f9aad842d18a1f
Andrey Sidorov <takandar@gmail.com>
2020-06-11 19:26:54 +0300

    image deletion logic

--- src/vue/image.vue
+++ src/vue/image.vue
@@ -1,6 +1,8 @@
 <template>
   <a class='b-image unprocessed' :href='node.attrs.src'>
-    <div class='controls'><div class='delete' /></div>
+    <div class='controls'>
+      <div class='delete' @click='remove' />
+    </div>
     <img :src='node.attrs.src'>
   </a>
 </template>
@@ -9,14 +11,16 @@
 export default {
   props: {
     node: { type: Object, required: true },
-    updateAttrs: { type: Function, required: true }
+    getPos: { type: Function, required: true },
+    view: { type: Object, required: true }
   },
   methods: {
-    onChange() {
-      debugger;
-      // this.updateAttrs({
-      //   done: !this.node.attrs.done,
-      // })
+    remove(e) {
+      e.preventDefault();
+      const position = this.getPos();
+      this.view.dispatch(
+        this.view.state.tr.delete(position, position + 1)
+      );
     }
   }
 };
