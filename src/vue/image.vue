c4cf3cbcae48a479105c79376b9daae28193c515
Andrey Sidorov <takandar@gmail.com>
2020-06-11 19:35:57 +0300

    image selection styles

--- src/vue/image.vue
+++ src/vue/image.vue
@@ -1,5 +1,9 @@
 <template>
-  <a class='b-image unprocessed' :href='node.attrs.src'>
+  <a
+    class='b-image unprocessed'
+    :class='{ "is-prosemirror-selected": selected }'
+    :href='node.attrs.src'
+  >
     <div class='controls'>
       <div class='delete' @click='remove' />
     </div>
@@ -12,10 +16,12 @@ export default {
   props: {
     node: { type: Object, required: true },
     getPos: { type: Function, required: true },
-    view: { type: Object, required: true }
+    view: { type: Object, required: true },
+    selected: { type: Boolean, required: true }
   },
   methods: {
     remove(e) {
+
       e.preventDefault();
       const position = this.getPos();
       this.view.dispatch(
@@ -25,3 +31,20 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+.b-image:hover,
+.b-image.is-prosemirror-selected {
+  outline: 3px solid #2b8acc;
+  z-index: 10;
+}
+.b-image:hover .controls,
+.b-image.is-prosemirror-selected .controls {
+  display: block;
+}
+
+.b-image:hover img,
+.b-image.is-prosemirror-selected img {
+  opacity: 1 !important;
+}
+</style>
