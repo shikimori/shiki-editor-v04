6c8b4c42769cb5e7c35328c79c86eaa9e6339170
Andrey Sidorov <takandar@gmail.com>
2020-06-12 15:54:01 +0300

    treat end of line in extractBbCode

--- src/markdown/tokenizer_helpers.js
+++ src/markdown/tokenizer_helpers.js
@@ -2,7 +2,11 @@ const MAX_BBCODE_SIZE = 10;
 
 export function extractBbCode(text, index) {
   for (let i = index + 1; i < index + MAX_BBCODE_SIZE; i++) {
-    if (text[i] === ']') {
+    const char = text[i];
+    const isEnd = char === '\n' || char === undefined;
+
+    if (isEnd) { return null; }
+    if (char === ']') {
       return text.slice(index, i + 1);
     }
   }
