b954cf93ee3844545c216e98ac4622d85b4cb63b
Andrey Sidorov <takandar@gmail.com>
2020-06-14 16:08:18 +0300

    eslint fix

--- src/markdown/bbcode_helpers.js
+++ src/markdown/bbcode_helpers.js
@@ -2,7 +2,7 @@ export function parseQuoteMeta(meta) {
   if (!meta) { return null; }
 
   const attributes = {};
-  const split = meta.split(';')
+  const split = meta.split(';');
 
   if (split.length === 1) {
     attributes.nickname = split[0];
