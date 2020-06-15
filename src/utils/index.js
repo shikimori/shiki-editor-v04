e4e786a324eabe767d2ad4800a2516d6492e21f6
Andrey Sidorov <takandar@gmail.com>
2020-06-15 19:16:49 +0300

    minMax fix

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,7 +2,7 @@ export { default as ComponentView } from './component_view';
 export { default as Emitter } from './emitter';
 export { default as ExtensionManager } from './extension_manager';
 export { default as buildMenu } from './build_menu';
-export { default as minMax } from './build_menu';
+export { default as minMax } from './min_max';
 export { default as getMarkAttrs } from './get_mark_attrs';
 export { default as getMarkRange } from './get_mark_range';
 export { default as getNodeAttrs } from './get_node_attrs';
