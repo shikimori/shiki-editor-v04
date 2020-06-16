840e1bc82d16acfb51454cd87c518aed2458515b
Andrey Sidorov <takandar@gmail.com>
2020-06-16 19:30:50 +0300

    show 2nd editor

--- demo/src/demo.vue
+++ demo/src/demo.vue
@@ -32,33 +32,10 @@ export default {
   },
   data: () => ({
     baseUrl: 'https://shikimori.one',
-    text1: `B[b]old tex[/b]t
-I[i]talic tex[/i]t
-U[u]nderlined tex[/u]t
-S[s]triked tex[/s]t
-C\`ode tex\`t
-L[url=https://github.com/shikimori/shiki-editor]ink tex[/url]t
-Image
-[img]https://kawai.shikimori.one/system/characters/original/166521.jpg?1591393014[/img] test [img]https://kawai.shikimori.one/system/users/x160/1.png?1591612283[/img]
-\`\`\`
-code block
-\`\`\`
-\`\`\`css
-css code block
-\`\`\`
-- Bulet List
-- def
-> - quoted list
-- > list quoted
-
-> Quote
-> > nope
-> yes
-
-[quote]Old style quote support[/quote]
-[quote=zxc]Old style quote with nickname[/quote]
-[quote=c1246;1945;Silentium°]Old style quote with user[/quote]`,
-    isColumn2: false,
+    text1: `test
+\`zxc\`
+`,
+    isColumn2: true,
     text2: `B[b]old tex[/b]t
 I[i]talic tex[/i]t
 U[u]nderlined tex[/u]t
