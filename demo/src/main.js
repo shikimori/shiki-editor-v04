acbab959a9e5392c32710a3690b49816c68238a7
Andrey Sidorov <takandar@gmail.com>
2020-06-18 21:34:19 +0300

    simple localization

--- demo/src/main.js
+++ demo/src/main.js
@@ -9,8 +9,20 @@ import '@/stylesheets/prosemirror.sass';
 Vue.config.productionTip = false;
 Vue.config.devtools = false;
 
+const TRANSLATIONS = {
+  'frontend.shiki_editor.strong': ' Жирный',
+  'frontend.shiki_editor.em': ' Курсив',
+  'frontend.shiki_editor.underline': ' Подчёркнутый',
+  'frontend.shiki_editor.deleted': ' Зачёркнутый',
+  'frontend.shiki_editor.undo': ' Отменить последнее изменение',
+  'frontend.shiki_editor.redo': ' Повторить последнее изменение',
+  'frontend.shiki_editor.prompt.image_url': ' Укажи URL картинки',
+  'frontend.shiki_editor.prompt.link_url': ' Укажи URL ссылки',
+  'frontend.shiki_editor.spoiler': 'Спойлер'
+};
+
 window.I18n = {
-  t: key => `:${key}`
+  t: key => TRANSLATIONS[key] || `:${key}`
 };
 
 new Vue({
