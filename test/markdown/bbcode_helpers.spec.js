deae8896c1c28bdd2e0c944bd0b58b18db9ea0bb
Andrey Sidorov <takandar@gmail.com>
2020-06-20 18:52:10 +0300

    parseQuoteMeta specs

---
+++ test/markdown/bbcode_helpers.spec.js
@@ -0,0 +1,44 @@
+import { expect } from 'chai';
+import {
+  parseQuoteMeta,
+  parseSpoilerMeta,
+  parseDivMeta
+} from '../../src/markdown/bbcode_helpers';
+
+describe('tokenizer_helpers', () => {
+  it('parseQuoteMeta', () => {
+    expect(parseQuoteMeta('qwe')).to.eql({
+      nickname: 'qwe'
+    });
+
+    expect(parseQuoteMeta('c1;1;qwe')).to.eql({
+      comment_id: 1,
+      user_id: 1,
+      nickname: 'qwe'
+    });
+
+    expect(parseQuoteMeta('m1;1;qwe')).to.eql({
+      message_id: 1,
+      user_id: 1,
+      nickname: 'qwe'
+    });
+
+    expect(parseQuoteMeta('t1;1;qwe')).to.eql({
+      topic_id: 1,
+      user_id: 1,
+      nickname: 'qwe'
+    });
+
+    expect(parseQuoteMeta('zxc;1;qwe')).to.eql({
+      user_id: 1,
+      nickname: 'qwe'
+    });
+  });
+
+  it('parseSpoilerMeta', () => {
+  });
+
+  it('parseDivMeta', () => {
+  });
+});
+
