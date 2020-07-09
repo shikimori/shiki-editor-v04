3477f33ba9e2205fd83358c6e8ee3999b82b5e14
Andrey Sidorov <takandar@gmail.com>
2020-07-09 19:18:00 +0300

    add more translations, add uploader styles

--- demo/src/main.js
+++ demo/src/main.js
@@ -1,9 +1,11 @@
+/* eslint max-len: 0 */
 import Vue from 'vue';
 import Demo from './demo.vue';
 
 import 'reset-css/reset.css';
 import '@/stylesheets/application.sass';
 import '@/stylesheets/prosemirror.sass';
+import 'shiki-utils/src/stylesheets/index.sass';
 
 Vue.config.productionTip = false;
 Vue.config.devtools = false;
@@ -18,7 +20,11 @@ const TRANSLATIONS = {
   'frontend.shiki_editor.prompt.image_url': ' URL картинки',
   'frontend.shiki_editor.prompt.link_url': ' URL ссылки',
   'frontend.shiki_editor.prompt.spoiler_label': 'Заголовок спойлера',
-  'frontend.shiki_editor.spoiler': 'Спойлер'
+  'frontend.shiki_editor.spoiler': 'Спойлер',
+  'frontend.lib.file_uploader.uploading_file': 'загрузка файла %{filename} (%{filesize} KB)',
+  'frontend.lib.file_uploader.uploading_files': 'загрузка файлов %{uploadedCount} из %{totalCount} (%{kbUploaded} / %{kbTotal} KB)',
+  'frontend.lib.file_uploader.drop_pictures_here': 'Перетаскивай сюда картинки',
+  'frontend.lib.file_uploader.file_type_not_allowed': 'Файл не является изображением'
 };
 
 window.I18n = {
