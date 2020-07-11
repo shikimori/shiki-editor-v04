872ee9a59343d37a25f074691532225621bc6a2d
Andrey Sidorov <takandar@gmail.com>
2020-07-11 15:06:51 +0300

    replace placeholder with uploaded image

--- src/commands/replace_upload_placeholder.js
+++ src/commands/replace_upload_placeholder.js
@@ -4,30 +4,17 @@ export default function(editor, { uploadId, response }) {
   const { state } = editor;
   const { dispatch } = editor.view;
   const pos = findPlaceholder(state, uploadId);
+  const url = editor.options.baseUrl + response.url;
 
   if (pos != null) {
     dispatch(
       state.tr
+        .replaceWith(pos, pos, editor.schema.nodes.image.create({ src: url }))
         .setMeta(uploadPlaceholder, { remove: { id: uploadId } })
     );
-        // .replaceWith(pos, pos, schema.nodes.image.create({src: url}))
   } else {
-    editor.commands.image(editor.options.baseUrl + response.url);
+    editor.commands.image(url);
   }
-
-  //   let pos = findPlaceholder(view.state, id)
-  //   // If the content around the placeholder has been deleted, drop
-  //   // the image
-  //   if (pos == null) return
-  //   // Otherwise, insert it at the placeholder's position, and remove
-  //   // the placeholder
-  //   view.dispatch(view.state.tr
-  //                 .replaceWith(pos, pos, schema.nodes.image.create({src: url}))
-  //                 .setMeta(placeholderPlugin, {remove: {id}}))
-  // }, () => {
-  //   // On failure, just clean up the placeholder
-  //   view.dispatch(tr.setMeta(placeholderPlugin, {remove: {id}}))
-  // })
 }
 
 
